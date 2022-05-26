333a688abcf83dcc1460c83f5b51be7ecb17923c
Esat35 <dugindugin35@gmail.com>
2022-05-26 21:48:36 +0300

    26.05.2022

--- src/test/java/day16_notations/C01_BeforeMethod_AfterMethod.java
+++ src/test/java/day16_notations/C01_BeforeMethod_AfterMethod.java
@@ -1,11 +1,13 @@
-package day16_natations;
+package day16_notations;
 
 import org.testng.annotations.Test;
 import utilities.TestBase;
 
 public class C01_BeforeMethod_AfterMethod extends TestBase {
 
-    //------------------------
+    // @BeforeMethod ve @AfterMethod  notasyonlari
+    // JUnit'deki @Before ve @After gibidir
+    // her test method'undan once ve sonra calisirlar
 
     @Test
     public void amazonTesti() {
