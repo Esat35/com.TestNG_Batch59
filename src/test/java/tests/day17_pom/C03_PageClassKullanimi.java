23edd3f70f4f8ca277d2d089503f67f825b275a1
Esat35 <dugindugin35@gmail.com>
2022-05-27 23:20:26 +0300

    27.05.2022

---
+++ src/test/java/tests/day17_pom/C03_PageClassKullanimi.java
@@ -0,0 +1,16 @@
+package tests.day17_pom;
+
+import org.testng.annotations.Test;
+
+public class C03_PageClassKullanimi {
+    @Test
+        public void test01() {
+            // facebook anasayfaya gidin
+            // cikarsa cookies kabul edin
+            // kullanici mail kutusuna rastgele bir isim yazdirin
+            // kullanici sifre kutusuna rastgele bir password yazdirin
+            // login butonuna basin
+            // giris yapilamadigini test edin
+
+    }
+}
---
+++ src/test/java/utilities/Driver.java
@@ -0,0 +1,38 @@
+package utilities;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class Driver {
+                /*
+                POM'de driver icin TestBase classine extends etmek yerine
+                Driver class'indan static methodlar kullanarak
+                driver olusturup ilgili ayarlarin yapilmasi ve en sonda
+                driverin kapatilmasi tercih edilmistir
+                 */
+
+    static WebDriver driver;
+
+    public static WebDriver getDriver() {
+        if (driver == null) {
+            WebDriverManager.chromedriver().setup();
+            driver = new ChromeDriver();
+            driver.manage().window().maximize();
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+        }
+        return driver;
+    }
+
+    public static void closeDriver() {
+        if (driver!=null){// drivera deger atanmıssa
+            driver.quit();
+            driver=null;  // sonrakı acmalarda sorun olmaması ıcın.
+        }
+
+    }
+
+
+}
