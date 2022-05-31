9e4a39096c395e45768e1a71f2701c84be9d7aed
Esat35 <dugindugin35@gmail.com>
2022-05-31 21:48:59 +0300

    31.05.2022

---
+++ src/test/java/tests/day19_smokeTest/PozitifLoginTest.java
@@ -0,0 +1,12 @@
+package tests.day19_smokeTest;
+
+import org.testng.annotations.Test;
+import utilities.Driver;
+
+public class PozitifLoginTest {
+    @Test
+    public void test01() {
+
+
+    }
+}
--- src/test/java/utilities/Driver.java
+++ src/test/java/utilities/Driver.java
@@ -9,11 +9,27 @@ import org.openqa.selenium.safari.SafariDriver;
 import java.time.Duration;
 public class Driver {
     /*
-    POM'de Driver icin TestBase class'ina extends etmek yerine
-    Driver class'indan static method'lar kullanarak
-    driver olusturup, ilgili ayarlarin yapilmasi
-    ve en sonda driver'in kapatilmasi tercih edilmistir.
+    POM'de driver icin TestBase classine extends etmek yerine
+    Driver class'indan static methodlar kullanarak
+    driver olusturup ilgili ayarlarin yapilmasi ve en sonda
+    driverin kapatilmasi tercih edilmistir
+
+    POM' de Driver classindaki getDriver() methodunun obje olusturularak
+    kullanilmasini engellemek icin
+    Singleton pattern kullanimi benimsenmistir
+
+    Singleton Pattern Tekli Kullanim, bir class'in farkli class'lardan
+    obje olusturularak kullanimini engellemek icin kullanilir
+
+    Bunu saglamak icin yapmamiz gereken sey oldukca basittir
+    Obje olusturmak icin kullanilan constructor i private yaptigimiz zaman baska class'lardan
+    driver classindan obje olusturulmasi mumkun olamaz
      */
+
+    private Driver() {
+
+    }
+
     static WebDriver driver;
     public static WebDriver getDriver(){
         if (driver==null) {
